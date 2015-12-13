e711fb05a937ef1ddca82c41b137223b03bf2621
Sébastien Combéfis <seb478@gmail.com>
2015-12-13 21:32:24 +0100

    Add personnality survey
    - Added questions and form to submit the answers

--- modules/surveys/client/controllers/surveys.client.controller.js
+++ modules/surveys/client/controllers/surveys.client.controller.js
@@ -3,4 +3,8 @@
 // Surveys controller
 angular.module('surveys').controller('SurveysController', ['$scope', '$stateParams', '$location', 'Authentication', function ($scope, $stateParams, $location, Authentication) {
 	$scope.authentication = Authentication;
+
+	$scope.submit = function() {
+		console.log('OK');
+	};
 }]);
