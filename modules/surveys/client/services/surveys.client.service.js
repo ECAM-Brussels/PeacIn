2ca138736e7c11ad187176f28dd85727b1dca299
Sébastien Combéfis <seb478@gmail.com>
2015-12-13 22:18:35 +0100

    Add service for the surveys module

---
+++ modules/surveys/client/services/surveys.client.service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Surveys service used for communicating with the surveys REST endpoints
+angular.module('surveys').factory('Surveys', ['$resource', function ($resource) {
+	return $resource('api/surveys/:surveyId', {
+		surveyId: '@_id'
+	}, {
+		update: {
+			method: 'PUT'
+		}
+	});
+}]);
