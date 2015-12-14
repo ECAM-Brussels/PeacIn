5f302fd926bfc8133765ef8ff98366cb688bfc07
Sébastien Combéfis <seb478@gmail.com>
2015-12-14 07:02:45 +0100

    Check personality test answers on client side

--- modules/surveys/client/controllers/surveys.client.controller.js
+++ modules/surveys/client/controllers/surveys.client.controller.js
@@ -5,6 +5,24 @@ angular.module('surveys').controller('SurveysController', ['$scope', '$statePara
 	$scope.authentication = Authentication;
 
 	$scope.submit = function() {
-		console.log('OK');
+		// Check scores for every question
+		$scope.error = {};
+		var valid = true;
+		for (var i = 0; i < 10; i++) {
+			var sum = 0;
+			for (var j = 0; j < 6; j++) {
+				if (this.answers && this.answers[i] && this.answers[i][j]) {
+					sum += this.answers[i][j];
+				}
+			}
+			if (sum !== 20) {
+				$scope.error[i] = 'Vous n\'avez pas attribué les 20 points pour cette question.';
+				valid = false;
+			}
+		}
+		// If data is valid, save to server
+		if (valid) {
+			// ...
+		}
 	};
 }]);
