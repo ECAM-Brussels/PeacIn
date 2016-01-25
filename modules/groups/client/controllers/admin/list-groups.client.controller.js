57a70a9d545683306031f6876bd9fea70ae38d73
Sébastien Combéfis <seb478@gmail.com>
2016-01-25 06:58:31 +0100

    Update groups module
    - Added a link to group creation page for admin
    - Added navigation breadcrumb on several pages

--- modules/groups/client/controllers/admin/list-groups.client.controller.js
+++ modules/groups/client/controllers/admin/list-groups.client.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('groups').controller('GroupListController', ['$scope', 'Groups', function ($scope, Groups) {
+angular.module('groups').controller('GroupListController', ['$scope', 'Groups', 'Authentication', function ($scope, Groups, Authentication) {
+	$scope.authentication = Authentication;
+
 	Groups.query(function (data) {
 		$scope.groups = data;
 	});
