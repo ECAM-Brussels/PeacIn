c7f732d7fff1ccd0d78a808137a34147fe4f783e
Sébastien Combéfis <seb478@gmail.com>
2016-01-30 18:00:17 +0100

    Add policies for server routes in meetings module

---
+++ modules/meetings/server/policies/meetings.server.policy.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var acl = require('acl');
+
+// Using the memory backend
+acl = new acl(new acl.memoryBackend());
+
+/**
+ * Invoke meetings permissions
+ */
+exports.invokeRolesPolicies = function() {
+	acl.allow([{
+		roles: ['admin', 'supervisor'],
+		allows: [{
+			resources: '/api/meetings',
+			permissions: '*'
+		}, {
+			resources: '/api/meetings/:meetingId',
+			permissions: '*'
+		}]
+	}, {
+		roles: ['teacher'],
+		allows: [{
+			resources: '/api/meetings',
+			permissions: ['get']
+		}, {
+			resources: '/api/meetings/:meetingId',
+			permissions: ['get']
+		}]
+	}]);
+};
+
+/**
+ * Check if meetings policy allows
+ */
+exports.isAllowed = function (req, res, next) {
+	var roles = (req.user) ? req.user.roles : [];
+
+	// Check for user roles
+	acl.areAnyRolesAllowed(roles, req.route.path, req.method.toLowerCase(), function (err, isAllowed) {
+		if (err) {
+			// An authorization error occurred
+			return res.status(500).send('Unexpected authorization error');
+		}
+		if (isAllowed) {
+			// Access granted! Invoke next middleware
+			return next();
+		}
+		return res.status(403).json({
+			message: 'User is not authorized'
+		});
+	});
+};
--- modules/meetings/server/routes/meetings.server.routes.js
+++ modules/meetings/server/routes/meetings.server.routes.js
@@ -3,16 +3,19 @@
 /**
  * Module dependencies
  */
-var meetings = require('../controllers/meetings.server.controller');
+var meetingsPolicy = require('../policies/meetings.server.policy'),
+	meetings = require('../controllers/meetings.server.controller');
 
 module.exports = function (app) {
 	// Meetings collection routes
 	app.route('/api/meetings')
+		.all(meetingsPolicy.isAllowed)
 		.get(meetings.list)
 		.post(meetings.create);
 
 	// Single meeting routes
 	app.route('/api/meetings/:meetingId')
+		.all(meetingsPolicy.isAllowed)
 		.get(meetings.read);
 
 	// Finish by binding the meeting middleware
