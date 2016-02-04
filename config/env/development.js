7e15f9df2b01ca23a9f8854a1fea29d59498e3c0
Sébastien Combéfis <seb478@gmail.com>
2016-02-04 16:56:00 +0100

    Update mailer configuration

--- config/env/development.js
+++ config/env/development.js
@@ -58,10 +58,15 @@ module.exports = {
   mailer: {
     from: process.env.MAILER_FROM || 'MAILER_FROM',
     options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      host: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      port: 587,
       auth: {
         user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
         pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+      },
+      secureConnection: 'false',
+      tls: {
+        ciphers: 'SSLv3'
       }
     }
   },
